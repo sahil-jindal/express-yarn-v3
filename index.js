a3a8866b6eb27913f82f5f8b67ba49bb366edb18
sahil-jindal <sahiljindal.thebest@gmail.com>
2022-10-08 05:14:39 +0530

    Added console log

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+console.log("Application started");
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
